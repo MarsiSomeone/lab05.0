4da4f3e01ae933291dc75c20cfcf5193dcf7a92d
MarsiSomeone <sirlovaolga@gmail.com>
2026-05-26 02:05:15 +0300

    clean

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marsi/MarsiSomeone/tasks/lab05/banking
-# Build directory: /home/marsi/MarsiSomeone/tasks/lab05/banking
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("tests")
--- tests/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/marsi/MarsiSomeone/tasks/lab05/banking/tests/tests[1]_tests.cmake")
-  include("/home/marsi/MarsiSomeone/tasks/lab05/banking/tests/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
