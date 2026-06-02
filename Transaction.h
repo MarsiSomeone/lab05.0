81500723a9ac49853b186f5d4b24054856c787cb
MarsiSomeone <sirlovaolga@gmail.com>
2026-06-03 00:56:26 +0300

    teeest

--- Transaction.h
+++ Transaction.h
@@ -10,10 +10,9 @@ class Transaction {
   int fee() const { return fee_; }
   void set_fee(int fee) { fee_ = fee; }
 
- private:
   void Credit(Account& accout, int sum);
   bool Debit(Account& accout, int sum);
-
+ private:
   // Virtual to test.
   virtual void SaveToDataBase(Account& from, Account& to, int sum);
 
